9c90ce3a3e906257107a61b4ccede1d5cb538bcb
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-02-24 21:44:30 +0700

    viết chương trình tính tổng  a và b cho layout đã tạo

--- AppCong/app/src/main/java/thebao/edu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/thebao/edu/appcong/MainActivity.java
@@ -1,7 +1,8 @@
 package thebao.edu.appcong;
 
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XulyCong(View view){
+        EditText SoA = findViewById(R.id.etdA);
+        EditText SoB = findViewById(R.id.edtB);
+        EditText kq = findViewById(R.id.edtKq);
+        //lấy dữ liệu ở điều khiển về số A
+        String strA = SoA.getText().toString();
+        //lấy dữ liệu ở điều khiển về số A
+        String strB = SoB.getText().toString();
+
+        // chuyển dữ liệu sang dạng số
+        int So_A = Integer.parseInt(strA);
+        // chuyển dữ liệu sang dạng số
+        int So_B = Integer.parseInt(strB);
+
+
+
+        // tính tổng
+        int Tong = So_A + So_B;
+        // hiện ra màn hình
+        //trước khi hiện th chuyển nó về dạng chuỗi để có thể hiện
+        String  strTong = String.valueOf(Tong);
+
+
+        kq.setText(strTong);
     }
 }
