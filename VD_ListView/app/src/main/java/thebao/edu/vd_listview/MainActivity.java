f345842193119686a781ece3305dee3a93ebe5b8
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-03-12 23:39:13 +0700

    Tạo listview đơn giản

--- VD_ListView/app/src/main/java/thebao/edu/vd_listview/MainActivity.java
+++ VD_ListView/app/src/main/java/thebao/edu/vd_listview/MainActivity.java
@@ -1,6 +1,11 @@
 package thebao.edu.vd_listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+        ListView lisviewNNLT;
+        ArrayList<String> dsNNLT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lisviewNNLT = findViewById(R.id.lvNNLT);
+        dsNNLT = new ArrayList<String>();
+        dsNNLT.add("Python");
+        dsNNLT.add("Java");
+        dsNNLT.add("C++");
+        dsNNLT.add("HTML");
+        dsNNLT.add("PHP");
+        ArrayAdapter<String> arrayAdapterNNLT;
+        arrayAdapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsNNLT);
+        lisviewNNLT.setAdapter(arrayAdapterNNLT);
+        lisviewNNLT.setOnItemClickListener(new  AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> paren, View view, int position, long id) {
+
+                //biến position đã chứa vị trí của item được click
+                String giatriduocchon = dsNNLT.get(position);
+
+
+                // làm theo yêu cầu bất kì đối với giá trị get được/
+                Toast.makeText(MainActivity.this, giatriduocchon, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
